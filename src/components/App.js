7de9d91c1547428c51a0d00aaf189e102c5d666c
kleber baldárrago salas <kleber@westpoint.io>
2021-11-06 14:09:34 -0300

    Feature: creacion de rutas. KB

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
+import { ContextProvider } from '../context/GlobalContext';
 import {
   Routes,
   Route,
 } from "react-router-dom";
+
 import Login from './login/Login';
 import Heading from './shared/Heading';
-import { ContextProvider } from '../context/GlobalContext';
+import Register from './register/Register';
+import Consultation from './consultation/Consultation';
+import Prediction from './prediction/Prediction';
 
 function App() {
   return (
@@ -14,6 +18,9 @@ function App() {
       <ContextProvider>
         <Routes>
           <Route path='/' element={<Login />} exact />
+          <Route path='/register' element={<Register />} exact />
+          <Route path='/consultation' element={<Consultation />} exact />
+          <Route path='/prediction' element={<Prediction />} exact />
         </Routes>
       </ContextProvider>
     </Fragment>
